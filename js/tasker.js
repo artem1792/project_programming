3fc86c8a59e217b407e21e6f4e602d3de6809fe4
artem1792 <aveartem123@gmail.com>
2025-05-06 11:18:04 +0500

    update

---
+++ js/tasker.js
@@ -0,0 +1,8 @@
+const btnCreateTask = document.getElementById("btn-create-task");
+const textTask = document.getElementById("text-task");
+const container = document.querySelector(".container");
+
+// Добавление новой задачи
+btnCreateTask.addEventListener("click", function(){
+    console.log("click");
+});
